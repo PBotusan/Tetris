140fd72034b5d04602c36c87f91f23e2374df9f0
Paul B <p_botusan@live.nl>
2020-11-24 16:20:59 +0100

    tetromino blok array

--- TetrisInConsoleApp/TetrisInConsoleApp.cpp
+++ TetrisInConsoleApp/TetrisInConsoleApp.cpp
@@ -3,8 +3,65 @@
 
 #include <iostream>
 
+using namespace std;
+
+
+wstring tetromino[7];
+
+// positionx, positiony
+int Rotate(int px, int py, int rotate) 
+{
+    switch (rotate % 4)
+    {
+        case 0: return py * 4 + px;         // 0 degrees
+        case 1: return 12 + py - (px * 4);  // 90 degrees
+        case 2: return 15 - (py * 4) - px;  // 180 degrees
+        case 3: return 3 - py + (px * 4);   // 270 degrees
+    }
+    return 0;
+
+}
+
 int main()
 {
+    tetromino[0].append(L"..x.");
+    tetromino[0].append(L"..x.");
+    tetromino[0].append(L"..x.");
+    tetromino[0].append(L"..x.");
+
+    tetromino[1].append(L"..x.");
+    tetromino[1].append(L".xx.");
+    tetromino[1].append(L".x..");
+    tetromino[1].append(L"....");
+
+    tetromino[2].append(L".x..");
+    tetromino[2].append(L".xx.");
+    tetromino[2].append(L"..x.");
+    tetromino[2].append(L"....");
+
+    tetromino[3].append(L"....");
+    tetromino[3].append(L".xx.");
+    tetromino[3].append(L".xx.");
+    tetromino[3].append(L"....");
+
+    tetromino[4].append(L"..x.");
+    tetromino[4].append(L".xx.");
+    tetromino[4].append(L"..x.");
+    tetromino[4].append(L"....");
+
+    tetromino[5].append(L"....");
+    tetromino[5].append(L".xx.");
+    tetromino[5].append(L"..x.");
+    tetromino[5].append(L"..x.");
+
+    tetromino[6].append(L"....");
+    tetromino[6].append(L".xx.");
+    tetromino[6].append(L".x..");
+    tetromino[6].append(L".x..");
+
+
+
+
     std::cout << "Hello World!\n";
 }
 
